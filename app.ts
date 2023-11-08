28fbe93fc3d8d1362449577613c5328a92a4f919
Albert-FFS <r.a.s.m.13@hotmail.com>
2023-11-07 19:47:00 -0800

    Correcion del efecto flip a nivel css en JS listo
    Siguiente en la version 4

--- app.ts
+++ app.ts
@@ -3,7 +3,9 @@ let back = document.querySelector('.face-back');
 let flip = document.querySelector('.book-content');
 let uno = document.querySelectorAll('.book');
 let portada = document.querySelectorAll('#portada');
-
+const node = document.createElement('div');
+node.style.transform ='RotateY(- deg)';
+node.style.transformOrigin ='left center'
 let contZindex = 2;
 let customZindex = 1;
 for (let index = 0; index < uno.length; index++) {
