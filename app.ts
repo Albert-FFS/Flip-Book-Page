a8e299c5c9ba4c299e86cd07c7a1e25e6d6bad71
Albert-FFS <r.a.s.m.13@hotmail.com>
2023-11-08 19:55:10 -0800

    Make Git Push

--- app.ts
+++ app.ts
@@ -1,16 +1,15 @@
-let front = document.querySelector('.face-front');
-let back = document.querySelector('.face-back');
-let flip = document.querySelector('.book-content');
-let uno = document.querySelectorAll('.book');
-let portada = document.querySelectorAll('#portada');
-const node = document.createElement('div');
-node.style.transform ='RotateY(- deg)';
-node.style.transformOrigin ='left center'
-let contZindex = 2;
-let customZindex = 1;
-for (let index = 0; index < uno.length; index++) {
-  // uno[index] as HTMLElement;
-  // uno[index].style.zIndex = customZindex;
-  customZindex--;
+const CreateNewBook = () => {
+  const element: HTMLDivElement = document.createElement('div');
+  element.classList.add(".book");
 }
-// 7:40
+const BookPropierties =
+  {
+    // Value of book can change
+    aspectRatio:"",
+    width:"",
+    perpective:"",
+    perspectiveOrigin:"",
+    // Cover
+    Cover:""
+
+  };
